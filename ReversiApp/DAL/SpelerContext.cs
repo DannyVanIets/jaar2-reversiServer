ae24b1c953392e88c6774e8c6c304216d535aba6
DannyVanIets <dannyvanbokhorst@live.nl>
2020-06-25 11:29:44 +0200

    Commit

--- ReversiApp/DAL/SpelerContext.cs
+++ ReversiApp/DAL/SpelerContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ReversiApp.Models;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ReversiApp.DAL
 {
-    public class SpelerContext : DbContext
+    public class SpelerContext : IdentityDbContext
     {
         public SpelerContext(DbContextOptions<SpelerContext> options) : base(options) { }
 

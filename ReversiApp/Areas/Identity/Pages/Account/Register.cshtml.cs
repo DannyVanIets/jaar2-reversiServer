8f39312a1720e27a9c3708846ecf32e14d92f2ba
Danny van Bokhorst <S1127947@student.windesheim.nl>
2023-04-06 10:02:10 +0200

    SetPassword geüpdatet
    Werkt prima, zorgt ervoor dat je niet hoger dan een bepaald getal kan, zelfs als je de HTML veranderd

--- ReversiApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ReversiApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,13 +52,14 @@ namespace ReversiApp.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
             [Display(Name = "Wachtwoord")]
+            [DataType(DataType.Password)]
+            [StringLength(maximumLength: 100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Wachtwoord bevestigen")]
+            [StringLength(maximumLength: 100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 

1428992a1f05e6d7d94f5daaccadfa2bc22ee9de
Danny van Bokhorst <S1127947@student.windesheim.nl>
2023-05-17 15:13:42 +0200

    Program has been cleared for take-off

--- ReversiApp/Program.cs
+++ ReversiApp/Program.cs
@@ -15,36 +15,37 @@ namespace ReversiApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-            //            Log.Logger = new LoggerConfiguration()
-            //                .WriteTo.Console()
-            //#if (DEBUG)
-            //                    .WriteTo.File($"Logs/Information/InformationLog-.txt",
-            //                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
-            //                    rollingInterval: RollingInterval.Minute)
-            //#endif
-            //                .WriteTo.File($"Logs/Warnings/WarningLog-.txt",
-            //                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
-            //                rollingInterval: RollingInterval.Minute)
-            //                .CreateLogger();
-            //            try
-            //            {
-            //                Log.Information("Application is starting up...");
-            //                Log.Warning("This test message can be safely ignored!");
-            //}
-            //catch (Exception e)
-            //{
-            //    Log.Fatal(e, "Something went wrong when starting up the application!");
-            //}
-            //finally
-            //{
-            //    Log.Information("Application is closing...");
-            //    Log.CloseAndFlush();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+#if (DEBUG)
+                    .WriteTo.File($"Logs/Information/InformationLog-.txt",
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                    rollingInterval: RollingInterval.Minute)
+#endif
+                .WriteTo.File($"Logs/Warnings/WarningLog-.txt",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
+                rollingInterval: RollingInterval.Minute)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Application is starting up...");
+                Log.Warning("This test message can be safely ignored!");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Something went wrong when starting up the application!");
+            }
+            finally
+            {
+                Log.Information("Application is closing...");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-        //.UseSerilog()
+        .UseSerilog()
         .ConfigureWebHostDefaults(webBuilder =>
         {
             //webBuilder.UseKestrel((options) =>
